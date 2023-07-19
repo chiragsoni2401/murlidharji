87e14fb6a4a49683856edde0128b91ce728a440c
raj.mistry@einfochips.com <raj.mistry@einfochips.com>
2023-07-19 14:18:28 +0530

    added required components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,19 @@ import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 
 import { IvyCarouselModule } from "angular-responsive-carousel";
+import { HallComponent } from './hall/hall.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { HomeComponent } from './home/home.component';
+import { MembersComponent } from './members/members.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { DonationComponent } from './donation/donation.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule, IvyCarouselModule],
-  declarations: [AppComponent, HelloComponent],
+  declarations: [AppComponent, HelloComponent, HallComponent, AboutUsComponent, GalleryComponent, ContactUsComponent, HomeComponent, MembersComponent, HeaderComponent, FooterComponent, DonationComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
