fde8a7daca65165eabaeec41872d396addf607d9
chiragsoni2401 <chiragsoni2401@gmail.com>
2023-07-20 11:17:19 +0530

    added required color and module for gallery

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from "@angular/core";
-import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppComponent } from "./app.component";
-import { HelloComponent } from "./hello.component";
-
-import { IvyCarouselModule } from "angular-responsive-carousel";
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { GalleryModule } from '@ngx-gallery/core';
+import { LightboxModule } from '@ngx-gallery/lightbox';
+import { IvyCarouselModule } from 'angular-responsive-carousel';
 import { HallComponent } from './hall/hall.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { GalleryComponent } from './gallery/gallery.component';
@@ -18,8 +20,22 @@ import { DonationComponent } from './donation/donation.component';
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, IvyCarouselModule, AppRoutingModule],
-  declarations: [AppComponent, HelloComponent, HallComponent, AboutUsComponent, GalleryComponent, ContactUsComponent, HomeComponent, MembersComponent, HeaderComponent, FooterComponent, DonationComponent],
-  bootstrap: [AppComponent]
+  imports: [BrowserModule, FormsModule, IvyCarouselModule, AppRoutingModule, BrowserAnimationsModule,
+    GalleryModule,
+    LightboxModule],
+  declarations: [
+    HeaderComponent,
+    AppComponent,
+    HelloComponent,
+    HallComponent,
+    AboutUsComponent,
+    GalleryComponent,
+    ContactUsComponent,
+    HomeComponent,
+    MembersComponent,
+    FooterComponent,
+    DonationComponent,
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
